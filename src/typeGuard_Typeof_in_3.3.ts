c356c57991eb49b07ca299908b0151943d7f49dc
SohelShahid117 <sohelshahid09@gmail.com>
2024-11-13 19:21:00 +0600

    done 3-3: Type guard using typeof & in

--- src/typeGuard_Typeof_in_3.3.ts
+++ src/typeGuard_Typeof_in_3.3.ts
@@ -2,12 +2,50 @@
     //3-3: Type guard using typeof & in
     //type guard or type narrowing
 
-    const add=(param1:string | number,param2:string | number):string | number=>{
-        return param1+param2;
-    }
-    const result = add(4,5);
+    type Alphanumeric = string | number;
 
-    //4min done
+    // const add=(param1:string | number,param2:string | number):string | number=>{
+    const add=(param1:Alphanumeric,param2:Alphanumeric):Alphanumeric=>{
+        // return param1+param2;
+        //here use typeof guard
+        if(typeof param1 ==="number" && typeof param2 ==="number"){
+            return param1 + param2;
+        }
+        else{
+            return param1.toString() + param2.toString();
+        }
+    }
+    const result1 = add(4,5);
+    console.log(result1);
+    const result2 = add("hello",5);
+    console.log(result2);
+
+    //now use in guard
+    type Normaluser = {
+        name:string;
+    }
+    type Adminuser = {
+        name:string;
+        role:string;
+    }
+    const getUser=(user:Normaluser | Adminuser)=>{
+        // return user.
+        if("role" in user){
+            console.log(`i am ${user.name} & my role is ${user.role}`)
+        }
+        else{
+            console.log(`i am ${user.name}`)
+        }
+    }
+   const Normaluser = {
+    name:"normal bhai"
+   }
+   const Adminuser={
+    name:"admin bhai",
+    role:"admin"
+   }
+   getUser(Normaluser);
+   getUser(Adminuser);
 
 
 
