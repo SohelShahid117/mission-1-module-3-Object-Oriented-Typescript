e669bef71a563bd41c8a75fc3b45df524c13ca3d
SohelShahid117 <sohelshahid09@gmail.com>
2024-11-14 07:12:23 +0600

    done 3-6: Getter and setter

--- dist/accessModifier_3.5.js
+++ dist/accessModifier_3.5.js
@@ -2,13 +2,41 @@
 {
     //3-5: Access modifiers
     class BankAccount {
+        // private property sodo tar nijer classei kaj korbe onno kono class || methode e kaj korbena
+        //protected others class || methode e kaj korbe
+        // protected _balance:number;
         constructor(id, name, balance) {
             this.id = id,
                 this.name = name,
-                this.balance = balance;
+                this._balance = balance;
+        }
+        addDeposit(amount) {
+            this._balance = this._balance + amount;
+        }
+        // private getBalance(){
+        getBalance() {
+            return this._balance;
         }
     }
-    const sohelerAccount = new BankAccount(117, "sohel", 20);
-    sohelerAccount.id = 118;
+    const sohelerAccount = new BankAccount(117, "sohel", 500);
+    // sohelerAccount.id=118;
+    // sohelerAccount.balance=0
+    // sohelerAccount.name="shahid";
     console.log(sohelerAccount.id);
+    // console.log(sohelerAccount.balance)
+    console.log(sohelerAccount.name);
+    sohelerAccount.addDeposit(1500);
+    sohelerAccount.addDeposit(1200);
+    const myBalance = sohelerAccount.getBalance();
+    // sohelerAccount.addDeposit(1200);
+    console.log(myBalance);
+    class StudentAccount extends BankAccount {
+        test() {
+            // this._balance=0;
+        }
+    }
+    const std1 = new StudentAccount(119, "ashraf", 60);
+    std1.test();
+    console.log("now balance is:" + sohelerAccount.getBalance());
+    console.log("now balance is:" + std1.getBalance());
 }
---
+++ src/getterSetter_3.6.ts
@@ -0,0 +1,41 @@
+{
+    //3-6: Getter and setter
+    class BankAccount{
+        id:number;
+        name:string;
+        balance:number;
+        constructor(id:number,name:string,balance:number){
+            this.id=id,
+            this.name=name,
+            this.balance=balance
+        }
+
+        //now use getter
+        get myBalance(){
+            return this.balance;
+        }
+
+        //now use setter
+        set sumirDeposit(depositAmmount:number){
+            this.balance=this.balance+depositAmmount;
+        }
+    }
+    const sumirAccount = new BankAccount(121,"sumi",50);
+    let sumirBal=sumirAccount.myBalance;
+    console.log(sumirBal)
+
+    //getter-setter e function er call hobe as like property not like as function()
+    // sumirAccount.sumirDeposit(100);
+    //function er get-set bosai dile seti property hoye jabe.
+    sumirAccount.sumirDeposit=100;
+    console.log(sumirBal)
+    console.log(sumirAccount.balance)
+
+
+
+
+
+
+
+
+}
