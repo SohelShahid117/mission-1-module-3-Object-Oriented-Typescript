bd8c5f7e37f451253b0af95bcd2f7388db823209
SohelShahid117 <sohelshahid09@gmail.com>
2024-11-14 06:56:35 +0600

    done 3-5: Access modifiers

--- src/accessModifier_3.5.ts
+++ src/accessModifier_3.5.ts
@@ -8,17 +8,24 @@
         // name:string;
         public readonly name:string;
         // public balance:number
-        private balance:number
+        private _balance:number; //underscore(_)use for private property
+        // private property sodo tar nijer classei kaj korbe onno kono class || methode e kaj korbena
+        //protected others class || methode e kaj korbe
+        // protected _balance:number;
+
         constructor(id:number,name:string,balance:number){
             this.id=id,
             this.name=name,
-            this.balance=balance
+            this._balance=balance
         }
-        addDeposit(amount:number){
-            this.balance=this.balance+amount;
+
+        public addDeposit(amount:number){
+            this._balance=this._balance+amount;
         }
-        getBalance(){
-            return this.balance;
+
+        // private getBalance(){
+        public getBalance(){
+            return this._balance;
         }
     }
 
@@ -36,8 +43,17 @@
     // sohelerAccount.addDeposit(1200);
     console.log(myBalance)
 
-    //10 min done
-
+    
+    class StudentAccount extends BankAccount{
+        test(){
+            // this._balance=0;
+        }
+    }
+    const std1 = new StudentAccount(119,"ashraf",60);
+    std1.test()
+    
+    console.log("now balance is:"+sohelerAccount.getBalance())
+    console.log("now balance is:"+std1.getBalance())
 
 
 
